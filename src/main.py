983816e23b1abd3714878c3d07c401c33923fb2c
S.Berkebile <samuel.berkebile@gmail.com>
2024-10-29 13:52:08 -0500

    I redid the main file, and moved most of the simulation code to simulation.py. main.py is still the runnable file. Let me know if you have any questions about what stuff does, I tried to use lots of comments lol. If you'd like to I think it would be good to make all the animals inherit from a main class too, and then have Carnivore and Herbivore classes that inherit and override certain methods.

--- src/main.py
+++ src/main.py
@@ -1,48 +1,33 @@
-import pygame
-from random import randint as rand
-import carnivore
-import math as m
-import time as t
-from carnivore import MaleCarn, FemaleCarn, carnivores, male_carns, female_carns # See carnivore.py
-from herbivore import MaleHerb, FemaleHerb, herbivores, male_herbs, female_herbs # See herbivore.py
+#!/usr/bin/env python3
+# I moved most of the code to its own class in simulation.py
+# but this file is still the one that runs the program.
+# It imports simulation.py and then creates an instance of a sim.
 
-pygame.init() # Initializes pygame, connects pygame's abstractions to the host computer.
+import simulation
+from src.carnivore import MaleCarn, FemaleCarn
+from src.herbivore import MaleHerb, FemaleHerb
 
+# Make an instance of a simulation
+sim = simulation.Simulation()
+sim.display_init(800, 600) # width and height, leave gui blank if you want a window
+sim.run() # leave speed blank for 1x speed
 
-SCREEN_WIDTH = 1000 # Setting up the screen for the ecosystem window.
-SCREEN_HEIGHT = 750
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-screen.fill((0,0,0))
+# HEADS UP: I only changed the main simulation code, I haven't done much
+# any work on the actual entities yet. But I'm thinking it would make
+# sense to have all the animals inherit from a parent class, (animal.py)
+# and then just use tags since presumably you'll be checking
 
+# THIS IS BROKEN RN BC THE CLASSES NEED TO BE EDITED FOR THE NEW SIM CODE
+# ALSO I AM GOING TO TRY USING SPRITE GROUPS FOR THE ORGANISMS BECAUSE
+# I THINK YOU CAN OVERRIDE THE UPDATE AND DRAW METHODS AS WELL AS HAVE
+# ACCESS TO THE groupCollide METHOD FOR INTERACTIONS.
 
 tiger = MaleCarn() # Initialize the starting organisms.
-tigeress = FemaleCarn()
+tigress = FemaleCarn()
 buck = MaleHerb()
 doe = FemaleHerb()
 
-male_carns.add(tiger) # Add the starting organisms to the right groups.
-female_carns.add(tigeress)
-male_herbs.add(buck)
-female_herbs.add(doe)
-carnivores.add(tiger, tigeress)
-herbivores.add(buck, doe)
-
-
-running = True # The main loop
-while running:
-    for event in pygame.event.get(): # Checking if the program window has been terminated.
-        if event.type == pygame.QUIT: # If so, exits main loop.
-            running = False
-        
-
-    screen.fill((0,0,0)) # Draws over past blits with a layer of black, the background color.
-
-    for carn in carnivores: # Updates all carnivores and draws them in the window.
-        carn.update() # See carnivore.Carnivore.update()
-        screen.blit(carn.surf, carn.rect)
-
-    for herb in herbivores: # Updates all herbivores and draws the in the window.
-        herb.update() # See carnibore.Carnivore.update()
-        screen.blit(herb.surf, herb.rect)
-
-    pygame.display.flip() # Applies all the changes made to the window since the last flip.
+sim.carnivores.add(tiger)
+sim.carnivores.add(tigress)
+sim.herbivores.add(buck)
+sim.herbivores.add(doe)
---
+++ src/simulation.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python3
+# the above is a shebang, for linux/unix OS's - it just tells the OS that
+# this is an executable python file.
+
+import pygame
+
+from src.carnivore import carnivores
+from src.herbivore import herbivores
+
+
+# This is basically just the entire main file in a class to keep it clean,
+# and also this would allow for parallel simulations via the threading
+# module in the future.
+class Simulation:
+
+    def __init__(self):
+        self.width = None
+        self.height = None
+        self.screen = None
+
+        self.speed = 1
+        self.is_running = False # this tells if the simulation loop is actually running
+        self.is_paused = False # this can be used to pause/play the simulation without breaking the loop
+        # I'm using a regular list for the animals, and just adding a tag to tell if they're
+        # male/female (you could probably do the same to tell if they're a carnivore/herbivore)
+        # You could use sprite Groups here, but the advantage of using a regular list is
+        # that you can do "invisible" animals that aren't sprite based as well, while also
+        # having more control over how they handle checks for collision, etc.
+        # (also I've never rly used sprite groups before so this is probably mostly preference lol)
+        self.carnivores = pygame.sprite.Group()
+        self.herbivores = pygame.sprite.Group()
+
+
+    # This just puts all the window setup code in a method, rather than __init__
+    # since the main class isn't *just* dealing with window but also the main loop
+    # and other things, it makes sense to put this separately. This would, for example,
+    # allow multiple simulations to be run at the same time without each having their own
+    # window.
+    def display_init(self, width: int, height: int, gui=True):
+        pygame.init() # Initializes pygame, connects pygame's abstractions to the host computer.
+        pygame.display.init()
+        self.width = width # Setting up the screen for the ecosystem window.
+        self.height = height
+        if gui:
+            self.screen = pygame.display.set_mode((self.width, self.height))
+            self.screen.fill((0,0,0)) # black
+
+
+    # This starts the main loop running, with a speed modifier that can be
+    # used to control how fast or slow the simulation should run.
+    def run(self, speed=1):
+        self.speed = speed
+        self.is_running = True
+        while self.is_running:
+            for event in pygame.event.get():  # Checking if the program window has been terminated.
+                if event.type == pygame.QUIT:  # If so, exits main loop.
+                    self.is_running = False
+                    self.is_paused = True
+
+            if not self.is_paused: # If the simulation isn't paused, update the entities.
+                self.update()
+
+            # Clear the screen, and draw the simulation
+            # The reason I split this into an update() and draw() methods is that you can
+            # update the sim without drawing it, for running multiple simulations etc.
+            # - and you can still have the simulation being rendered even when it's paused
+            self.screen.fill((0, 0, 0))
+            self.draw()
+
+
+    # Updates the simulation, using the speed property to tell
+    # how much to update them by.
+    def update(self):
+        self.carnivores.update(self.speed)
+        self.herbivores.update(self.speed)
+
+
+    # Draws the simulation to the screen. Adding a camera may be helpful in the future
+    # As it would allow the simulation to go beyond the bounds of the window, and
+    # then the view can be controlled independently.
+    def draw(self):
+        # I'm passing the screen instance to each carnivore object so you can reuse
+        # the drawing code in the carnivore class. This will make it easier to made
+        # updates to the drawing code, and to find it as the project grows.
+        carnivores.draw(self.screen)
+        herbivores.draw(self.screen)
+        pygame.display.update() # Update and flip are basically the same
